f5f49ca5d40ef4c7606dd035077a6c06daf9c63d
JoseCobena <jose.cobena@pucese.edu.ec>
2021-06-25 18:03:08 -0500

    App completa

---
+++ CRUD1/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD1.Models;
+
+namespace CRUD1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(CRUD1.Models.User userModel)
+        {
+            using (ElManabaEntities db = new ElManabaEntities()){
+                var userDetails = db.Users.Where(x => x.EmaisAddress == userModel.EmaisAddress && x.Password == userModel.Password).FirstOrDefault();
+                if(userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Email o contraseña inválida.";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.IdUser;
+                    Session["Name"] = userDetails.Name;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+        public ActionResult LogOut()
+        {
+            int userID = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ CRUD1/Controllers/ProductsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD1.Models;
+
+namespace CRUD1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ElManabaEntities db = new ElManabaEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Category);
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdCategory = new SelectList(db.Categories, "IdCategory", "Name");
+            return View();
+        }
+
+        // POST: Products/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdProduct,Name,PurchasePrice,SalePrice,Description,IdCategory")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdCategory = new SelectList(db.Categories, "IdCategory", "Name", product.IdCategory);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdCategory = new SelectList(db.Categories, "IdCategory", "Name", product.IdCategory);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdProduct,Name,PurchasePrice,SalePrice,Description,IdCategory")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdCategory = new SelectList(db.Categories, "IdCategory", "Name", product.IdCategory);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
